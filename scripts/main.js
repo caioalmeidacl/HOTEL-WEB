8ac60478d9492419c122abdb561ee246407565af
Caio Carvalho <caioalmeidaacl@gmail.com>
2025-02-15 18:34:20 -0300

    rooms v1

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,58 @@
-fetch("http://localhost:8000/rooms.json")
-  .then((response) => {
+async function getRooms() {
+  try {
+    const response = await fetch("http://localhost:8000/rooms.json");
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.json();
-  })
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Fetch error:", error));
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+
+async function displayAvailableRooms() {
+  const data = await getRooms();
+  const div = document.querySelector("#rooms");
+
+  for (const room of data.rooms) {
+    if (room.isAvailable) {
+      const template = `
+        <div class="room">
+            <div class="picture">
+            <span class="category">${room.category}</span>
+            <img src="${room.image}" />
+            </div>
+            <div class="room-info">
+            <h1>${room.name}</h1>
+            <h2>${room.currency}${room.price}<span class="period">${room.period}</span></h2>
+            </div>
+            <div class="room-measures">
+            <span>
+            <img src="${data.icons.size}" />
+            <h3>${room.details.size}</h3>
+            </span>
+            <span>
+            <img src="${data.icons.capacity}" />
+            <h3>${room.details.capacity}</h3>
+            </span>
+            <span>
+            <img src="${data.icons.beds}" />
+            <h3>${room.details.beds}</h3>
+            </span>
+            <span>
+            <img src="${data.icons.bathrooms}" />
+            <h3>${room.details.bathrooms}</h3>
+            </span>
+            <input type="button" value="Book Now" class="button" />
+            </div>
+        </div>
+    `;
+      div.innerHTML += template;
+    }
+  }
+}
+
+displayAvailableRooms();
